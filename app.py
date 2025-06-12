7bac4c4d4df5532641dd5232c5f04e323db46cde
Saketh Sampath Vinay <sakethsampthvinay46@gmail.com>
2025-06-12 11:56:33 +0530

    learned sessions-cookies

--- app.py
+++ app.py
@@ -1,17 +1,79 @@
-from flask import Flask, request, make_response, render_template, redirect, url_for
+from flask import Flask, request, make_response, render_template, redirect, url_for, session
 
 app = Flask(__name__, template_folder="templates")
+app.secret_key = 'SOME KEY'
+
+
 
 @app.route('/')
 def index():
-    value = "Flask Application"
-    result = [10, 20, 30, 40, 50]
-    return render_template('index.html', myValue = value, myResult = result);
+    return render_template(template_name_or_list = 'index.html', message = 'Index')
 
-@app.route("/other")
-def other():
-    some_text = "Hello, World"
-    return render_template('index.html', some_text = some_text);
+@app.route('/set_data')
+def set_data():
+    session['name'] = 'Mike'
+    session['other'] = 'Hello World'
+    return render_template('index.html', message= 'Session data set')
+
+
+@app.route('/get_data')
+def get_data():
+    name = session['name']
+    other = session['other']
+    return render_template(template_name_or_list = 'index.html', message = f"Name: {name}, Other: {other}")
+
+
+@app.route('/clear_session')
+def clear_session():
+    session.clear()
+    return render_template(template_name_or_list = 'index.html', message = "Session Data Cleared")
+
+@app.route('/set_cookie')
+def set_cookie():
+    response = make_response(render_template(template_name_or_list = 'index.html', message = "Cookie set."))
+    response.set_cookie(key =  'cookie_name', value = 'cookie_value' )
+    return response
+
+
+@app.route('/get_cookie')
+def get_cookie():
+    cookie_value = request.cookies['cookie_name']
+    return render_template(template_name_or_list = 'index.html', message = f'Cookie Value: {cookie_value}')
+
+@app.route('/remove_cookie')
+def remove_cookie():
+    response = make_response(render_template(template_name_or_list = 'index.html', message = "Cookie Removed"))
+    response.set_cookie(key =  'cookie_name', expires = 0 )
+    return response
+
+
+# @app.route('/')
+# def index():
+#     value = "Flask Application"
+#     result = [10, 20, 30, 40, 50]
+#     return render_template('index.html', myValue = value, myResult = result);
+
+# @app.route("/other")
+# def other():
+#     some_text = "Hello, World"
+#     return render_template('index.html', some_text = some_text);
+
+@app.route('/form-route', methods=['GET', 'POST'])
+def routes():
+    if request.method == 'GET':
+        return render_template('index.html') 
+    elif request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == 'saketh' and password == 'password':
+            return 'Success'
+        else:
+            return 'Failure'
+
+@app.route('/file_upload', methods=['POST'])
+def file_upload():
+    return ""
 
 @app.route('/redirect_endpoint')
 def redirect_endpoint():
